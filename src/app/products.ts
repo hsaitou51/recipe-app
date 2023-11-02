a3ca7399f8a5576a287ce905668512c7dcfd1c08
hideaki51 <hs.wgp.46@gmail.com>
2023-11-02 11:41:25 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+    {
+      name: '齋藤　翔太',
+      aeg: '8歳',
+      description: '南本町小学校　3年1組',
+      img: './assets/img/IMG_5399.JPG',
+      heading1: 'バスケットボール🏀',
+      heading2: 'パソコン教室　　💻',
+      heading3: 'ハゼ釣り　　　　🎣',
+      headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+    },
+    {
+      name: '齋藤　陽七太',
+      aeg: '4歳',
+      description: '二葉幼稚園　年少菊組',
+      img: './assets/img/IMG_0965.JPG',
+      heading1: 'ドングリ拾い　🌰',
+      heading2: '滑り台　　　　🛝',
+      heading3: '拳銃　　　　　🔫',
+      headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+    },
+    {
+      name: '齋藤　梓',
+      aeg: '43歳',
+      description: '船橋市海神町2-3-21',
+      img: './assets/img/IMG_7758.JPG',
+      heading1: 'お菓子作り　　　🍰',
+      heading2: '歌声　　　　　　🎤',
+      heading3: 'ピアノ　　　　　🎹',
+      headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+    },
+    {
+      name: '齋藤　英朗',
+      aeg: '52歳',
+      description: '船橋市海神町2-3-21',
+      img: './assets/img/IMG_4089.JPG',
+      heading1: '食事　　　　　　🍽️',
+      heading2: 'ゴルフ　　　　　⛳️',
+      heading3: '翔太、陽七太、梓😃',
+      headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+      headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+      headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+    }
+  ];
+  
